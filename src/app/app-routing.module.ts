fc03e9b6f45051e4d031c5b466505195bd351b3b
LuckyTemitope <65639852+LuckyTemitope@users.noreply.github.com>
2022-03-08 08:37:27 -0500

    - Initial Commit - Added routing modules and paths to different pages - Created Navbars - Created Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { StandingsComponent } from './standings/standings.component';
+import { SchedulesComponent } from './schedules/schedules.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }, {
+    path: 'home',
+    component: HomeComponent
+  },{
+    path: 'schedule',
+    component: SchedulesComponent
+  }, {
+    path: 'standings',
+    component: StandingsComponent
+  }, {
+    path: 'contact',
+    component: ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SchedulesComponent } from './schedules/schedules.component';
+import { StandingsComponent } from './standings/standings.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SchedulesComponent,
+    StandingsComponent,
+    FooterComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
