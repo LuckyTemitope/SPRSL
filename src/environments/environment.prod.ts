73b29ff27f388d71f3654b8305cc13acb15ab942
Karan Pal Singh <kdsgyt@gmail.com>
2022-03-29 13:47:52 -0400

    Added the firebase authentication data for production app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'sprsl-42b18',
+    appId: '1:112917368566:web:b61aca52115f32475826d7',
+    storageBucket: 'sprsl-42b18.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDGyf_p7njN8u48PgQBD2_imVGa9LLrzZ4',
+    authDomain: 'sprsl-42b18.firebaseapp.com',
+    messagingSenderId: '112917368566',
+    measurementId: 'G-6K2MCHTFER',
+  },
+  production: false
 };
